2f50c7773eefd5cbe9bf1ebc20b92d44de6cf9e3
Valentina1927 <deisyvalentinatorres@gmail.com>
2023-01-18 18:46:44 +0100

    ensayando esta pagina de nuevo !!!

---
+++ src/main/java/com/mycompany/lambide/PanelInvetarios.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.lambide;
+
+/**
+ *
+ * @author Admin
+ */
+public class PanelInvetarios {
+    
+    public static void main(String[]args){
+    
+    System.out.println("Intentado conectar");
+}
+}
+
